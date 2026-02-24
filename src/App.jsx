54257d94cfb0a63c119b34a957e551f5b70adf90
Paras Jain <parasjain182006@gmail.com>
2026-02-24 18:09:53 +0530

    Add routing and protect main manager route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Manager from './components/Manager'
-import Footer from './components/footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import Navbar from "./components/Navbar";
+import Manager from "./components/Manager";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Navbar/>
- 
-     <Manager/>
-   
-    <Footer/>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Manager />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
